ab9b47b310429ce46f5c2a1f6f70092e3111fd62
Alejandro Maimus <alejandromaimus147@gmail.com>
2019-05-03 16:36:09 -0400

    Timeline double accordion

---
+++ src/app/timeline/events.json.ts
@@ -0,0 +1,33 @@
+export const EVENTS:any = [
+  {
+  title: "Lorem ipsum dolor sit amet",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quisquam. Blanditiis soluta nihil cumque qui, in illum mollitia. Perspiciatis omnis dolores, aut aliquid ab corporis eum delectus sint, corrupti iusto tenetur alias, facilis quisquam autem harum perferendis quo natus vitae unde quis sit nesciunt. At temporibus delectus quo laudantium voluptate!",
+  time: "July 2002",
+  },
+
+  {
+  title: "Lorem ipsum dolor sit amet",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quisquam. Blanditiis soluta nihil cumque qui, in illum mollitia. Perspiciatis omnis dolores, aut aliquid ab corporis eum delectus sint, corrupti iusto tenetur alias, facilis quisquam autem harum perferendis quo natus vitae unde quis sit nesciunt. At temporibus delectus quo laudantium voluptate!",
+  time: "July 2002",
+  },
+
+  {
+  title: "Lorem ipsum dolor sit amet",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quisquam. Blanditiis soluta nihil cumque qui, in illum mollitia. Perspiciatis omnis dolores, aut aliquid ab corporis eum delectus sint, corrupti iusto tenetur alias, facilis quisquam autem harum perferendis quo natus vitae unde quis sit nesciunt. At temporibus delectus quo laudantium voluptate!",
+  time: "July 2002",
+  },
+
+  {
+  title: "Lorem ipsum dolor sit amet",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quisquam. Blanditiis soluta nihil cumque qui, in illum mollitia. Perspiciatis omnis dolores, aut aliquid ab corporis eum delectus sint, corrupti iusto tenetur alias, facilis quisquam autem harum perferendis quo natus vitae unde quis sit nesciunt. At temporibus delectus quo laudantium voluptate!",
+  time: "July 2002",
+  },
+
+
+  {
+  title: "Lorem ipsum dolor sit amet",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quisquam. Blanditiis soluta nihil cumque qui, in illum mollitia. Perspiciatis omnis dolores, aut aliquid ab corporis eum delectus sint, corrupti iusto tenetur alias, facilis quisquam autem harum perferendis quo natus vitae unde quis sit nesciunt. At temporibus delectus quo laudantium voluptate!",
+  time: "July 2002",
+  },
+  
+];
--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EVENTS } from './events.json';
+declare var $:any;
 
 @Component({
   selector: 'app-timeline',
@@ -6,23 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline.component.css']
 })
 export class TimelineComponent implements OnInit {
+
+
   title:string = "Timeline"
-  events: object = [
-    {content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quisquam. Blanditiis soluta nihil cumque qui, in illum mollitia. Perspiciatis omnis dolores, aut aliquid ab corporis eum delectus sint, corrupti iusto tenetur alias, facilis quisquam autem harum perferendis quo natus vitae unde quis sit nesciunt. At temporibus delectus quo laudantium voluptate!",
-    time: "July 2002",
-    title: "Lorem ipsum dolor sit amet"},
-    {content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quisquam. Blanditiis soluta nihil cumque qui, in illum mollitia. Perspiciatis omnis dolores, aut aliquid ab corporis eum delectus sint, corrupti iusto tenetur alias, facilis quisquam autem harum perferendis quo natus vitae unde quis sit nesciunt. At temporibus delectus quo laudantium voluptate!",
-    time: "July 2001",
-    title: "Lorem ipsum dolor sit amet"},
-    {content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quisquam. Blanditiis soluta nihil cumque qui, in illum mollitia. Perspiciatis omnis dolores, aut aliquid ab corporis eum delectus sint, corrupti iusto tenetur alias, facilis quisquam autem harum perferendis quo natus vitae unde quis sit nesciunt. At temporibus delectus quo laudantium voluptate!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quisquam. Blanditiis soluta nihil cumque qui, in illum mollitia. Perspiciatis omnis dolores, aut aliquid ab corporis eum delectus sint, corrupti iusto tenetur alias, facilis quisquam autem harum perferendis quo natus vitae unde quis sit nesciunt. At temporibus delectus quo laudantium voluptate!",
-    time: "July 2000",
-    title: "Lorem ipsum dolor sit amet"},
-    {content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quisquam. Blanditiis soluta nihil cumque qui, in illum mollitia. Perspiciatis omnis dolores, aut aliquid ab corporis eum delectus sint, corrupti iusto tenetur alias, facilis quisquam autem harum perferendis quo natus vitae unde quis sit nesciunt. At temporibus delectus quo laudantium voluptate!",
-    time: "July 1999",
-    title: "Lorem ipsum dolor sit amet"}]
+  events: object = EVENTS
   constructor() { }
 
   ngOnInit() {
+    $('.ui.accordion').accordion();
   }
 
 }
