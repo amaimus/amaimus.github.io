d74681684e8678f072241f9859e192782dddec81
Alejandro Maimus <alejandromaimus147@gmail.com>
2019-05-06 16:40:44 -0400

    Tecnologies component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimelineComponent } from './timeline/timeline.component';
+import { TecnologiesComponent } from './tecnologies/tecnologies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimelineComponent
+    TimelineComponent,
+    TecnologiesComponent
   ],
   imports: [
     BrowserModule,
